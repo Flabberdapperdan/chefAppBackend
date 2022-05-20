08bcd2677ff5bc2e7eea9b95660c271e3d755577
Matthijs <snijdermatthijs@gmail.com>
2022-05-20 09:42:29 +0200

    first commit backend

---
+++ src/main/java/com/chefApp/demo/ChefAppApplication.java
@@ -0,0 +1,13 @@
+package com.chefApp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefAppApplication.class, args);
+	}
+
+}
