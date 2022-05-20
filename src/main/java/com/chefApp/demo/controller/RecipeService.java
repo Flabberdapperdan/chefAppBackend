401eec3fbc4de33a0c802be220a224314494e707
KasperMoliin <moliin@hotmail.com>
2022-05-20 13:50:40 +0200

    third commit

--- src/main/java/com/chefApp/demo/controller/RecipeService.java
+++ src/main/java/com/chefApp/demo/controller/RecipeService.java
@@ -9,7 +9,13 @@ public class RecipeService {
 
     @Autowired
     RecipeRepository r;
-
+    
+    public void getOne(long id) {
+        Recipe foundRecipe = r.findById(id);
+    }
+    public void createOne(Recipe recipe) {
+        r.save(recipe);
+    }
     public Recipe safeOne(Recipe recipe) {
         return recipe;
     }
--- src/main/java/com/chefApp/demo/rest/RecipeEndpoint.java
+++ src/main/java/com/chefApp/demo/rest/RecipeEndpoint.java
@@ -21,7 +21,6 @@ public class RecipeEndpoint {
         Recipe r = new Recipe();
         r.setId(1);
         r.setName("pasta");
-        r.setWeight(100);
         return Arrays.asList(r);
     }
 
@@ -30,3 +29,38 @@ public class RecipeEndpoint {
 
     }
 }
+//package com.chefApp.demo.rest;
+//
+//import com.chefApp.demo.controller.RecipeService;
+//import com.chefApp.demo.model.Ingredient;
+//import com.chefApp.demo.model.Recipe;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@RestController
+//public class IngredientEndpoint {
+//
+//  @Autowired
+//  RecipeService service;
+//
+//  @GetMapping("ingredient/{id}")
+//  public void getIngredientById(@PathVariable("id") int id) {
+//
+//
+//  }
+//
+//  @PostMapping("ingredient")
+//  public List<Ingredient> createNewIngredient() {
+//      System.out.println("post ingredient");
+//      Ingredient ingredient = new Ingredient();
+//      ingredient.setId(1);
+//      ingredient.setName("boerenkool");
+//      return Arrays.asList(ingredient);
+//  }
+//}
