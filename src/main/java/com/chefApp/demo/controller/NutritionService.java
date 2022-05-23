16173f90b35ee776e245cac1e3f4599087c6e8e3
KasperMoliin <moliin@hotmail.com>
2022-05-23 13:57:39 +0200

    WIP endpoints

--- src/main/java/com/chefApp/demo/controller/NutritionService.java
+++ src/main/java/com/chefApp/demo/controller/NutritionService.java
@@ -2,12 +2,14 @@ package com.chefApp.demo.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.chefApp.demo.model.Nutrition;
 
+import utilities.ValidationException;
 
+@Service
 public class NutritionService {
 	@Autowired
 	NutritionRepository r;
@@ -23,6 +25,19 @@ public class NutritionService {
 	public void createOne(Nutrition nutrition) {
 	r.save(nutrition);
 	}
+	
+	 public Object updateOne(Nutrition newNutrition, long id) {
+	        Nutrition oldNutrition = this.getOne(id).get();
+	        if (newNutrition.getName().length() > 0) {
+	            oldNutrition.setName(newNutrition.getName());
+	        } else {
+	            return new ValidationException();
+	        }
+	       
+	        
+	        Nutrition updatedNutrition= r.save(oldNutrition);
+	        return updatedNutrition;
+	 }
 	public void deleteOne(long id) {
 	r.deleteById(id);
 	}
