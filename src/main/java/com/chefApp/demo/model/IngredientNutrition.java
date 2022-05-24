9c154312e2e24f1a2c1096ea3f154b7009d7e878
Matthijs <snijdermatthijs@gmail.com>
2022-05-24 20:42:53 +0200

    add IngredientNutrition entity

--- src/main/java/com/chefApp/demo/model/IngredientNutrition.java
+++ src/main/java/com/chefApp/demo/model/IngredientNutrition.java
@@ -9,13 +9,57 @@ public class IngredientNutrition {
     @GeneratedValue( strategy = GenerationType.AUTO )
     private long id;
 
-    @ManyToMany
+    @ManyToOne
     @MapsId("ingredientId")
-    @JoinColumn(name = ingredient_id)
+    @JoinColumn(name = "ingredient_id")
     Ingredient ingredient;
 
-    @ManyToMany
+    @ManyToOne
     @MapsId("nutritionId")
     @JoinColumn(name = "nutrition_id")
     Nutrition nutrition;
+
+    Integer amount;
+    String metric;
+
+    // getters and setters \\
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Nutrition getNutrition() {
+        return nutrition;
+    }
+
+    public void setNutrition(Nutrition nutrition) {
+        this.nutrition = nutrition;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getMetric() {
+        return metric;
+    }
+
+    public void setMetric(String metric) {
+        this.metric = metric;
+    }
 }
